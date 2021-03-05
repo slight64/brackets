a1bf4d94982aaf96a18734a8575a1029ec500b8e
slight64 <slight1989@gmail.com>
2021-03-05 20:54:23 +0400

    feat: complete check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    bracketsConfig = bracketsConfig.flat(Infinity);
+    str = str.split('');
+    let stack = [];
+    for (let i = 0; i < str.length; i++) {
+
+
+        if (bracketsConfig.indexOf(str[i]) % 2 === 0) {
+            stack.push(str[i]);
+            console.log('push', stack);
+        } else if (stack.pop() === bracketsConfig[bracketsConfig.indexOf(str[i - 1])]) {
+            stack.pop();
+            console.log(stack);
+        }
+        return stack.length === 0;
+    }
+};
